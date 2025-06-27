8a2dd9e4f21172eb46f85db84b0ae294e5466202
joaocosta9 <johnydacosta@hotmail.com>
2025-06-27 19:16:59 +0100

    fix query

--- src/hooks/useEthuiAbi.ts
+++ src/hooks/useEthuiAbi.ts
@@ -9,7 +9,7 @@ const useEthuiAbi = ({ address }: { address: Address }) => {
   const { data: walletClient } = useWalletClient();
 
   return useQuery({
-    queryKey: ["abi", address, isEthui],
+    queryKey: ["abi", address],
     queryFn: async () => {
       const abi = (await walletClient?.request({
         method: "ethui_getContractAbi",
