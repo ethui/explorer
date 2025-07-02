7346d8a398b9633ab7011ec3d765eedb48a9778f
joaocosta9 <johnydacosta@hotmail.com>
2025-07-02 16:18:27 +0100

    wip

--- src/routes/rpc.$rpc/_l/tx.$tx.tsx
+++ src/routes/rpc.$rpc/_l/tx.$tx.tsx
@@ -1,14 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { type Hash, isHash, Transaction, TransactionReceipt } from "viem";
+import { type Hash, isHash } from "viem";
 import { useTransaction, useTransactionReceipt } from "wagmi";
 import PageContainer from "#/components/PageContainer";
-import {
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-  Card,
-} from "@ethui/ui/components/shadcn/card";
+import { Card } from "@ethui/ui/components/shadcn/card";
+import { LoadingSpinner } from "#/components/LoadingSpinner";
 
 export const Route = createFileRoute("/rpc/$rpc/_l/tx/$tx")({
   component: RouteComponent,
@@ -22,57 +17,60 @@ export const Route = createFileRoute("/rpc/$rpc/_l/tx/$tx")({
 
 function RouteComponent() {
   const { tx } = Route.useLoaderData();
-  const { data: transaction } = useTransaction({
-    hash: tx,
-  });
-  const { data: receipt } = useTransactionReceipt({
-    hash: tx,
-  });
 
   return (
     <PageContainer header="Transaction Details">
-      <TransactionDetails
-        transaction={transaction as Transaction}
-        receipt={receipt}
-      />
+      <TransactionDetails tx={tx} />
     </PageContainer>
   );
 }
 
-function TransactionDetails({
-  transaction,
-  receipt,
-}: {
-  transaction: Transaction;
-  receipt?: TransactionReceipt;
-}) {
+function TransactionDetails({ tx }: { tx: Hash }) {
+  const { data: transaction, isLoading: isTransactionLoading } = useTransaction(
+    {
+      hash: tx,
+    },
+  );
+
+  const { data: receipt, isLoading: isReceiptLoading } = useTransactionReceipt({
+    hash: tx,
+  });
+
   return (
     <Card className="p-8 flex flex-col rounded-2xl border shadow-md mt-4">
-      <TransactionLabelValue
-        label="Transaction Hash"
-        value={transaction.hash}
-      />
-      {receipt && (
-        <TransactionLabelValue
-          label="Status"
-          value={receipt.status === "success" ? "Success" : "Failed"}
-        />
+      {isTransactionLoading || isReceiptLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <TransactionLabelValue
+            label="Transaction Hash"
+            value={transaction?.hash}
+          />
+          {receipt && (
+            <TransactionLabelValue
+              label="Status"
+              value={receipt.status === "success" ? "Success" : "Failed"}
+            />
+          )}
+        </>
       )}
     </Card>
   );
-}
 
-function TransactionLabelValue({
-  label,
-  value,
-}: {
-  label: string;
-  value: string | React.ReactNode;
-}) {
-  return (
-    <div className="flex flex-row items-center pb-6">
-      <div className="text-sm text-muted-foreground w-1/4">{label}</div>
-      <div className="text-sm w-3/4">{value}</div>
-    </div>
-  );
+  function TransactionLabelValue({
+    label,
+    value,
+  }: {
+    label: string;
+    value: string | React.ReactNode;
+  }) {
+    return (
+      <div className="flex flex-row items-start pb-6">
+        <div className="text-sm text-muted-foreground w-1/4 pr-4 flex-shrink-0">
+          {label}
+        </div>
+        <div className="text-sm flex-1 break-all">{value}</div>
+      </div>
+    );
+  }
 }
