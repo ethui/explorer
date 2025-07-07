e0421e1b06c72f28e0d72c07aad200aa5f806da6
joaocosta9 <johnydacosta@hotmail.com>
2025-07-07 16:57:53 +0100

    tx missing input and logs

--- src/routes/rpc.$rpc/_l/tx.$tx.tsx
+++ src/routes/rpc.$rpc/_l/tx.$tx.tsx
@@ -1,21 +1,18 @@
 import { Card } from "@ethui/ui/components/shadcn/card";
+
 import { createFileRoute } from "@tanstack/react-router";
 import clsx from "clsx";
 import { format } from "date-fns";
 import titleize from "titleize";
-import { type Hash, formatEther, isHash } from "viem";
+import { type Hash, type Transaction, formatEther, isHash } from "viem";
 import { useBlock, useTransaction, useTransactionReceipt } from "wagmi";
 import { Chip } from "#/components/Chip";
-import { ContractExecutionForm } from "#/components/Forms/ContractInteractionForm/components/ContractExecutionForm";
-import { useContractExecution } from "#/components/Forms/ContractInteractionForm/hooks/useContractExecution";
 import { LinkText } from "#/components/LinkText";
 import { LoadingSpinner } from "#/components/LoadingSpinner";
 import PageContainer from "#/components/PageContainer";
 import { Tabs } from "#/components/Tabs";
-import useAbi from "#/hooks/useAbi";
 import { stringifyWithBigInt } from "#/utils/formatters";
 import { formatRelativeTime } from "#/utils/time";
-import { getDecodedFunctionInput } from "#/utils/transaction";
 
 export const Route = createFileRoute("/rpc/$rpc/_l/tx/$tx")({
   component: RouteComponent,
@@ -35,7 +32,6 @@ function RouteComponent() {
       <Tabs
         tabs={[
           { label: "Overview", component: <TransactionDetails tx={tx} /> },
-          { label: "Input Data", component: <InputDetails tx={tx} /> },
           { label: "Logs", component: <Logs tx={tx} /> },
         ]}
       />
@@ -69,12 +65,12 @@ function TransactionDetails({ tx }: { tx: Hash }) {
   return (
     <Card className="flex flex-col rounded-2xl border p-8 pb-2 shadow-md">
       <TransactionLabelValue
-        label="Transaction Hash"
+        label="Transaction Hash:"
         value={transaction?.hash}
       />
       {receipt && (
         <TransactionLabelValue
-          label="Status"
+          label="Status:"
           value={
             <Chip
               variant={receipt.status === "success" ? "success" : "error"}
@@ -86,7 +82,7 @@ function TransactionDetails({ tx }: { tx: Hash }) {
         />
       )}
       <TransactionLabelValue
-        label="Block"
+        label="Block:"
         value={
           <LinkText
             to="/rpc/$rpc/block/$blockNumber"
@@ -100,13 +96,13 @@ function TransactionDetails({ tx }: { tx: Hash }) {
       />
       {block && (
         <TransactionLabelValue
-          label="Timestamp"
+          label="Timestamp:"
           value={<Timestamp timestamp={block.timestamp} />}
         />
       )}
 
       <TransactionLabelValue
-        label="From"
+        label="From:"
         value={
           <LinkText
             to="/rpc/$rpc/address/$address"
@@ -120,7 +116,7 @@ function TransactionDetails({ tx }: { tx: Hash }) {
 
       {transaction?.to && (
         <TransactionLabelValue
-          label="To"
+          label="To:"
           value={
             <LinkText
               to="/rpc/$rpc/address/$address"
@@ -134,13 +130,13 @@ function TransactionDetails({ tx }: { tx: Hash }) {
       )}
 
       <TransactionLabelValue
-        label="Value"
+        label="Value:"
         value={<span>{formatEther(transaction?.value)} ETH</span>}
       />
 
       {receipt && (
         <TransactionLabelValue
-          label="Transaction Fee"
+          label="Transaction Fee:"
           value={
             <span>
               {formatEther(receipt.gasUsed * receipt.effectiveGasPrice)} ETH
@@ -149,16 +145,58 @@ function TransactionDetails({ tx }: { tx: Hash }) {
         />
       )}
       <TransactionLabelValue
-        label="Gas Price"
+        label="Gas Price:"
         value={<span>{formatEther(transaction.gasPrice ?? 0n)} ETH</span>}
       />
       <TransactionLabelValue label="Nonce" value={transaction.nonce} />
       <TransactionLabelValue
-        label="Transaction Type"
+        label="Transaction Type:"
         value={<Chip variant="default">{titleize(transaction.type)}</Chip>}
+      />
+
+      <TransactionLabelValue
+        label="Input Data:"
+        value={<InputDetails transaction={transaction} />}
       />
     </Card>
   );
+
+  function InputDetails({ transaction }: { transaction: Transaction }) {
+    // const [displayMode, setDisplayMode] = useState<
+    //   "default" | "utf8" | "original"
+    // >("default");
+
+    return (
+      <div className="flex flex-col gap-2">
+        <div className="min-h-20 w-full rounded-md border border-r-4 bg-accent p-4">
+          {transaction.input}
+        </div>
+        {/* <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              size="sm"
+              className="w-fit bg-accent text-xs focus:outline-none focus:ring-0 focus-visible:ring-0"
+            >
+              <span className="capitalize">{displayMode}</span>
+              <ChevronDown className="ml-2 h-3 w-3" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-32">
+            <DropdownMenuItem onClick={() => setDisplayMode("default")}>
+              Default
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setDisplayMode("utf8")}>
+              UTF-8
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setDisplayMode("original")}>
+              Original
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu> */}
+      </div>
+    );
+  }
 
   function TransactionLabelValue({
     label,
@@ -168,7 +206,7 @@ function TransactionDetails({ tx }: { tx: Hash }) {
     value: string | React.ReactNode;
   }) {
     return (
-      <div className="flex flex-row items-center pb-6">
+      <div className="flex flex-row pb-6">
         <div className="w-1/4 flex-shrink-0 pr-4 text-muted-foreground text-sm">
           {label}
         </div>
@@ -176,68 +214,6 @@ function TransactionDetails({ tx }: { tx: Hash }) {
       </div>
     );
   }
-}
-
-function InputDetails({ tx }: { tx: Hash }) {
-  const { data: transaction, isLoading: isTransactionLoading } = useTransaction(
-    {
-      hash: tx,
-    },
-  );
-
-  const { abi } = useAbi({ address: transaction?.to ?? "0x" });
-  const execution = useContractExecution(transaction?.to ?? "0x");
-
-  if (isTransactionLoading) {
-    return <LoadingSpinner />;
-  }
-
-  if (!transaction) {
-    return <div>Transaction not found</div>;
-  }
-
-  if (!transaction.to) {
-    return <div>Contract deployment transaction</div>;
-  }
-
-  if (transaction.input === "0x") {
-    return <div>This was a native transfer transaction</div>;
-  }
-
-  if (!abi) {
-    return (
-      <InputDetailsCard className="max-w-[1000px]">
-        <pre className="mt-2 w-full whitespace-pre-wrap break-all rounded bg-muted p-4">
-          {transaction.input}
-        </pre>
-      </InputDetailsCard>
-    );
-  }
-
-  const decodedInput =
-    abi && transaction.input
-      ? getDecodedFunctionInput({
-          abi,
-          input: transaction.input,
-        })
-      : null;
-
-  if (!decodedInput) {
-    return <div>No function input found</div>;
-  }
-
-  return (
-    <InputDetailsCard>
-      {decodedInput && (
-        <ContractExecutionForm
-          execution={execution}
-          address={transaction.to}
-          abiFunction={decodedInput.abiFunction}
-          defaultCalldata={transaction.input}
-        />
-      )}
-    </InputDetailsCard>
-  );
 }
 
 function Logs({ tx }: { tx: Hash }) {
