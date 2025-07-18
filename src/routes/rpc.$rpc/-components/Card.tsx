c72cbaae02baad9f2254aa25405f19f9d59c456f
joaocosta9 <johnydacosta@hotmail.com>
2025-07-18 16:44:21 +0100

    remove footer

--- src/routes/rpc.$rpc/-components/Card.tsx
+++ src/routes/rpc.$rpc/-components/Card.tsx
@@ -14,7 +14,7 @@ import { formatEth } from "#/utils/formatters";
 interface CardProps {
   title: string;
   children: React.ReactNode;
-  footerLink: {
+  footerLink?: {
     text: string;
     to: string;
   };
@@ -26,15 +26,19 @@ export function Card({ title, children, footerLink }: CardProps) {
       <CardHeader className="border-b p-5">
         <CardTitle>{title}</CardTitle>
       </CardHeader>
-      <CardContent className="flex-1 px-4 py-0">{children}</CardContent>
-      <CardFooter className="flex items-center justify-center rounded-b-2xl border-t bg-accent p-4">
-        <LinkText
-          className="!text-muted-foreground hover:!text-solidity-value flex flex-row items-center gap-2"
-          to={footerLink.to}
-        >
-          {footerLink.text} <ArrowRight className="h-4 w-4" />
-        </LinkText>
-      </CardFooter>
+      <CardContent className={clsx("flex-1 px-4 pb-3", footerLink && "py-0")}>
+        {children}
+      </CardContent>
+      {footerLink && (
+        <CardFooter className="flex items-center justify-center rounded-b-2xl border-t bg-accent p-4">
+          <LinkText
+            className="!text-muted-foreground hover:!text-solidity-value flex flex-row items-center gap-2"
+            to={footerLink.to}
+          >
+            {footerLink.text} <ArrowRight className="h-4 w-4" />
+          </LinkText>
+        </CardFooter>
+      )}
     </ShadcnCard>
   );
 }
