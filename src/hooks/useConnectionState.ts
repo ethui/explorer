33907aa9a91f9eb40ac8063c17a5e8732dc94b85
joaocosta9 <joaomcosta6@gmail.com>
2025-08-29 19:51:42 +0100

    fix (#223)

--- src/hooks/useConnectionState.ts
+++ src/hooks/useConnectionState.ts
@@ -1,9 +1,14 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { useWatchBlockNumber } from "wagmi";
 import { useConnectionStore } from "../store/connection";
 
 export function useConnectionState({ rpc }: { rpc: string }) {
-  const { setState } = useConnectionStore();
+  const { setState, reset } = useConnectionStore();
+
+  useEffect(() => {
+    reset();
+    setState({ rpc });
+  }, [rpc, setState, reset]);
 
   const onBlockNumber = useCallback(
     (b: bigint) => {
