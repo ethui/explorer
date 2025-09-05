5770f87ae6ceb1e40b96883ade33474ba4e710b6
joaocosta9 <joaomcosta6@gmail.com>
2025-09-05 11:54:39 +0100

    add (#228)

--- src/components/AddressLink.tsx
+++ src/components/AddressLink.tsx
@@ -1,15 +1,23 @@
+import { FileCode2 } from "lucide-react";
 import type { Address } from "viem";
 import useAddressAlias from "#/hooks/useAddressAlias";
+import { useIsContract } from "#/hooks/useIsContract";
 import { truncateHex } from "#/utils/hash";
 import { LinkText } from "./LinkText";
 
 interface AddressLinkProps {
   address: Address;
   text?: string;
+  showIcon?: boolean;
 }
 
-export function AddressLink({ address, text }: AddressLinkProps) {
+export function AddressLink({
+  address,
+  text,
+  showIcon = true,
+}: AddressLinkProps) {
   const { data: alias } = useAddressAlias({ address });
+  const { isContract } = useIsContract(showIcon ? address : undefined);
 
   const displayText = alias || text || truncateHex(address);
 
@@ -18,7 +26,12 @@ export function AddressLink({ address, text }: AddressLinkProps) {
       to="/rpc/$rpc/address/$address"
       params={{ address }}
       tooltip={address}
+      className="flex flex-row items-center gap-1"
     >
+      {showIcon && isContract && (
+        <FileCode2 size={16} className="text-muted-foreground" />
+      )}
+
       {displayText}
     </LinkText>
   );
