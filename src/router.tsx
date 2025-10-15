a7f092e5c1bf7404106780d5f25787397c8350e3
joaocosta9 <johnydacosta@hotmail.com>
2025-10-15 15:19:03 +0100

    upgrade

--- src/router.tsx
+++ src/router.tsx
@@ -10,7 +10,7 @@ export function getRouter() {
 
   const router = createRouter({
     routeTree,
-    context: { queryClient } as any,
+    context: { queryClient },
     defaultPreload: "intent",
     defaultErrorComponent: DefaultCatchBoundary,
     defaultNotFoundComponent: () => <NotFound />,
