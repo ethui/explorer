4a5e209815717158f8fc60def3f2d2161b32decd
joaocosta9 <johnydacosta@hotmail.com>
2025-10-16 13:53:33 +0100

    upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   server: {
@@ -13,6 +14,9 @@ export default defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tanstackStart(),
+    nitroV2Plugin({
+      preset: "vercel",
+    }),
     tailwindcss(),
     viteReact(),
   ],
